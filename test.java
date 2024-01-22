c60022739befc5c2ed5b88dedcbba5658849b779
Ayush Raj <rajayush1412@gmail.com>
2024-01-22 20:51:42 +0530

    int to character conversion

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test {
+    public static void main (String [] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner (System.in);
+        int num;
+        num = sc.nextInt ();
+        sc.close ();
+        char ch = (char) num;
+
+        System.out.println ("The character quivalent of the number " + num + " is " + ch);
+    }
+}
