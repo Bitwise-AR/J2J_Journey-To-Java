4f41fbdbafbd5d8150e4036443991ae9018b1974
Ayush Raj <rajayush1412@gmail.com>
2024-02-05 23:38:00 +0530

    multi-dimensional array basics

---
+++ multi.java
@@ -0,0 +1,25 @@
+public class multi {
+    public static void main(String[] args) {
+        // way1
+        int arr[][] = {{1,2,3},
+                    {4,5,6},
+                    {7,8,9}};
+
+        // way2
+        // int arr[][] = new int[3][3];
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        // using for each loop
+        // for(int[] a : arr){
+        //     for(int e : a){
+        //         System.out.print(e);
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
