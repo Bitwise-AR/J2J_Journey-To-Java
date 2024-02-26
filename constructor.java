6f4f6ec8e78e48bca3922d4c2afdf25afebe483f
Ayush Raj <rajayush1412@gmail.com>
2024-02-26 19:31:26 +0530

    updates

---
+++ constructor.java
@@ -0,0 +1,17 @@
+class A{
+    int a;
+    A(int a){
+        System.out.println("parameterize");
+    }
+}
+
+public class constructor{
+    public static void main(String[] args){
+        // A obj = new A();
+        System.out.println("bhabhi 1");
+    }
+    public static void main(int a, String[] args){
+        // A obj = new A();
+        System.out.println("bhabhi 2");
+    }  
+}
